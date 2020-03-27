3d06dd2290847d192007096c657ce8505e18b1de
Derek Nowicki <derek@knowikiapps.com>
2020-03-26 19:25:02 -0700

    add datbase connector

---
+++ models/db.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+
+// Create a connection to the database
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST_IP,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+// open the MySQL connection
+pool.getConnection(function(err, connection){
+  if (err) throw err; // not connected!
+  console.log('connected as id ' + connection.threadId);
+});
+
+// connection.connect(error => {
+//   if (error) throw error;
+//   console.log("Successfully connected to the database.");
+// });
+
+module.exports = pool;
